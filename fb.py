637dc9af222c5a03962fa93fc20b8b38560e2a04
DongGunYoon <donggun961108@gmail.com>
2021-03-25 19:33:43 +0900

    refactor: Changed the structure of func to List Comprehension.
    Changed the structure of do_fizzbuzz function from for loop to list comprehension to increase the readability and performance.

--- fb.py
+++ fb.py
@@ -1,13 +1,6 @@
 def do_fizzbuzz(num):
-	for i in range(1, num+1):
-		if i%15==0:
-			print("FIZZBUZZ")
-		elif i%3==0:
-			print("FIZZ")
-		elif i%5==0:
-			print("BUZZ")
-		else:
-			print(i)
+	answer = ['FIZZBUZZ' if i%15==0 else 'FIZZ' if i%3==0 else "BUZZ" if i%5==0 else i for i in range(1, num+1)]
+	print(answer)
 
 if __name__=='__main__':
 	do_fizzbuzz(int(input()))
